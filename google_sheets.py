7b4006ad0197e93c6dbb4e925613c04e116e9f88
Eric Peter <epeter@kilo.local>
2017-05-02 16:15:50 -0700

    Google sheets
    Add in driving / walking time + update error handling

--- google_sheets.py
+++ google_sheets.py
@@ -1,17 +1,20 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-GOOGLE_API_KEY_FILE = 'client_secret.json'
+import google_maps
+import time
+
 try:
     from private import *
 except Exception:
     pass
 
-# use creds to create a client to interact with the Google Drive API
-scope = ['https://spreadsheets.google.com/feeds']
-credentials = ServiceAccountCredentials.from_json_keyfile_name(GOOGLE_API_KEY_FILE, scope)
-gc = gspread.authorize(credentials)
+GOOGLE_API_KEY_FILE = 'client_secret.json'
 
 def open_sheet():
+    # use creds to create a client to interact with the Google Drive API
+    scope = ['https://spreadsheets.google.com/feeds']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(GOOGLE_API_KEY_FILE, scope)
+    gc = gspread.authorize(credentials)
     return gc.open_by_key(GOOGLE_SHEET_ID).worksheet('Scraped_Data')
 
 def add_new_record(sheet, record_to_add):
@@ -36,19 +39,16 @@ def add_new_record(sheet, record_to_add):
             record_to_add['fb_stop'],
             record_to_add['fb_dist'],
             record_to_add['should_include'],
+            record_to_add['address'],
+            record_to_add['adi_drivetime'],
+            record_to_add['google_walktime'],
+            record_to_add['fb_walktime']
             ]
     try:
         sheet.insert_row(row, new_row)
-        sheet.update_cell(new_row, 19, '=hyperlink("https://www.google.com/maps/place/"&geoCodeLatLong(D%d,E%d), geoCodeLatLong(D%d,E%d))'%(new_row, new_row, new_row, new_row))
-        sheet.update_cell(new_row, 20, '=importxml("http://maps.googleapis.com/maps/api/directions/xml?origin=" &D%d&","&E%d  & "&destination=555 California St, San Francisco, CA 94104&sensor=false&alternatives=false&departure_time=1493827772&mode=driving","//leg/duration/text")'%(new_row, new_row))
-        sheet.update_cell(new_row, 21, '=importxml("http://maps.googleapis.com/maps/api/directions/xml?origin="&D%d&","&E%d&"&destination="&index(Shuttlses!$H$2:$H$30, match($N%d,Shuttlses!$G$2:$G$30, 0))&"&sensor=false&alternatives=false&departure_time=1493827772&mode=walking","//leg/duration/text")'%(new_row, new_row, new_row))
-        sheet.update_cell(new_row, 22, '=importxml("http://maps.googleapis.com/maps/api/directions/xml?origin="&$D%d&","&$E%d&"&destination="&index(Shuttlses!$D$2:$D$18, match($P%d,Shuttlses!$C$2:$C$18, 0))&"&sensor=false&alternatives=false&departure_time=1493827772&mode=walking","//leg/duration/text")'%(new_row, new_row, new_row))
-    except:
-        try:
-            sheet.insert_row(row, new_row)
-            sheet.update_cell(new_row, 19, '=hyperlink("https://www.google.com/maps/place/"&geoCodeLatLong(D%d,E%d), geoCodeLatLong(D%d,E%d))'%(new_row, new_row, new_row, new_row))
-            sheet.update_cell(new_row, 20, '=importxml("http://maps.googleapis.com/maps/api/directions/xml?origin=" &D%d&","&E%d  & "&destination=555 California St, San Francisco, CA 94104&sensor=false&alternatives=false&departure_time=1493827772&mode=driving","//leg/duration/text")'%(new_row, new_row))
-            sheet.update_cell(new_row, 21, '=importxml("http://maps.googleapis.com/maps/api/directions/xml?origin="&D%d&","&E%d&"&destination="&index(Shuttlses!$H$2:$H$30, match($N%d,Shuttlses!$G$2:$G$30, 0))&"&sensor=false&alternatives=false&departure_time=1493827772&mode=walking","//leg/duration/text")'%(new_row, new_row, new_row))
-            sheet.update_cell(new_row, 22, '=importxml("http://maps.googleapis.com/maps/api/directions/xml?origin="&$D%d&","&$E%d&"&destination="&index(Shuttlses!$D$2:$D$18, match($P%d,Shuttlses!$C$2:$C$18, 0))&"&sensor=false&alternatives=false&departure_time=1493827772&mode=walking","//leg/duration/text")'%(new_row, new_row, new_row))
-        except:
-            print 'error'
+    except Exception as e:
+        print e
+        # try again just in case - sometimes Google tells you to wait 30 seconds and retry
+        time.sleep(30)
+        sheet.insert_row(row, new_row)
+        #if we fail again, let it fail
